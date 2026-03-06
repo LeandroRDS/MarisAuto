24818fc8ca93fc611ecda71e7b55aadf6d62d8af
Leandro Rodrigues DS <76908370+LeandroRDS@users.noreply.github.com>
2026-03-05 23:23:08 -0300

    desenvolvendo login (3) corigindo a falha de  alteraçao de tabela(eu escrevi alert em vez de alter)

--- alterar_tabela.php
+++ alterar_tabela.php
@@ -1,7 +1,11 @@
 <?php 
+    
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
 include("includes/conexao.php");
 
-$sql = "ALERT TABLE carros ADD usuario_id INT";
+$sql = "ALTER TABLE carros ADD usuario_id INT";
 
 if(mysqli_query($conn, $sql)){
     echo "coluna criada com suceso";
