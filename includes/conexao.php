ef6f63ea355d486f4ae0980478ee604be015ea6d
Leandro Rodrigues DS <76908370+LeandroRDS@users.noreply.github.com>
2026-03-06 22:47:56 -0300

    tentando arrumar um geito de terate o cadigo mais facil

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,13 +1,22 @@
 <?php
-$host = "sql100.infinityfree.com";
-$user = "if0_40632939";
-$pass = "Lrdsamdsjcpr123";
-$bd = "if0_40632939_bd_anuncios";
 
-$conn = new mysqli($host, $user, $pass, $bd);
+if($_SERVER['SERVER_NAME'] == "localhost"){
+
+    $conn = new mysqli("localhost","root","","marisauto_1");
+
+}else{
+
+    $conn = new mysqli(
+        "sql312.infinityfree.com",
+        "if0_40632939",
+        "Lrdsamdsjcpr123",
+        "if0_40632939_bd_anuncios"
+    );
+
+}
 
 if($conn->connect_error){
-    die("erro na conexãoo" . $conn->connect_error);
+    die("Erro de conexão: " . $conn->connect_error);
 }
- //echo "conectado com sucesso"
+
 ?>
