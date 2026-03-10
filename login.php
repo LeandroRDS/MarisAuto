347cab0d0e1b614951ff968c90362c9b4016fc6a
leandro <leandrords23012002@gmail.com>
2026-03-10 13:16:50 -0300

    feita a estilização da tela de login fazendo testes

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <link rel="stylesheet" href="style/imports.css">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/login.css">
     <title>login</title>
 </head>
 <body>
