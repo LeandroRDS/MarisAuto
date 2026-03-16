71d7fe132349d494fd70673b12f02dcb74986ac4
leandro <leandrords23012002@gmail.com>
2026-03-16 20:21:11 -0300

    corrigindo erros de digitação e de sitaxe no arquivo index.html clickcompar.js o ddd estava errado

--- javascript/clikcomprar.js
+++ javascript/clikcomprar.js
@@ -1,7 +1,7 @@
 function abrirWhatsapp(vendedor, carro) {
     fetch("resistrar_leads.php?vendedor=" + vendedor + "&carro=" + carro);
     let mensagem = "Olá, tenho interesse no carro " + carro + ". Consultor " + vendedor;
-    let url = "https://wa.me/5531992643867?text=" + encodeURIComponent(mensagem);
+    let url = "https://wa.me/5547992643867?text=" + encodeURIComponent(mensagem);
     window.open(url, "_blank");
 }
 
