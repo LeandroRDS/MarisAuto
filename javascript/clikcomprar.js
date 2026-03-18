332b25790f1ac8359a5927cae04e73028e5bd74a
leandro <leandrords23012002@gmail.com>
2026-03-17 23:43:10 -0300

    criada a funcionalidade que salva o leads e ser respctivos vendedores no bd

--- javascript/clikcomprar.js
+++ javascript/clikcomprar.js
@@ -4,24 +4,22 @@ document.querySelectorAll('.btn_whasapp').entriesforEach(btn => {
         let descricao = this.dataset.descricao;
         let carro_id = this.dataset.carro_id;
 
+        abrirWhatsapp(vendedor_id, descricao, carro_id);
 
-        abrirWhatsapp(vendedor, descricao, carro_id);
-
-       
     })
 })
 
-function abrirWhatsapp(vendedor_id, descricao, carro_id ) {
+function abrirWhatsapp(vendedor_id, descricao, carro_id) {
     fetch("registrar_leads.php", {
         method: "POST",
         headers: {
             "content-Type": "application/x-www-form-urlencode"
         },
-        body: "vendedor_id" + vendedor_id + "descricao" + descricao + "carro_id" + carro_id
-    });
-
-    let mensagem = "Olá, tenho interesse no carro " + carro + ". Consultor " + vendedor;
-
-    let url = "https://wa.me/5547992643867?text=" + encodeURIComponent(mensagem);
-    window.open(url, "_blank");
+        body: "vendedor_id=" + vendedor_id + "&descricao=" + descricao + "&carro_id=" + carro_id
+    })
+        .then(() => {
+            let mensagem = "Olá, tenho interesse no carro " + descricao + ". Consultor " + vendedor;
+            let url = "https://wa.me/5547992643867?text=" + encodeURIComponent(mensagem);
+            window.open(url, "_blank");
+        });
 }
