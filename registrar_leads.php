240abfbc454579e048167a0e9a176a24f96693cd
leandro <leandrords23012002@gmail.com>
2026-03-18 23:51:57 -0300

    testando pq nao salva o lead no bd 5

--- registrar_leads.php
+++ registrar_leads.php
@@ -1,8 +1,8 @@
 <?php
 include("includes/conexao.php"); //colocando a conexaõ do bd nessa arquivo
 
-$vendedor_id = $_POST['vendedor_id'] ?? null;//pegando o valor que esta na variavel vendedor_id no clikcimprar.ls atravez do metodo post.
-$carro_id = $_POST['carro_id '] ?? null;//pegano o valor que esta na variavel carro_id no arquivo clikcomprar.js atravez do metodo post e salvando ela na variavel carro_is para colocala no banco de dados.
+$vendedor_id = $_POST['vendedor_id'];//pegando o valor que esta na variavel vendedor_id no clikcimprar.ls atravez do metodo post.
+$carro_id = $_POST['carro_id'];//pegano o valor que esta na variavel carro_id no arquivo clikcomprar.js atravez do metodo post e salvando ela na variavel carro_is para colocala no banco de dados.
 
 
 
