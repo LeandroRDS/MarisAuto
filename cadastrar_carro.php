9db26ffe74ea32e19c5f10f9401a058db536841a
Leandro Rodrigues DS <76908370+LeandroRDS@users.noreply.github.com>
2026-04-06 21:37:29 -0300

    parcialmente estilizada a tela login

--- cadastrar_carro.php
+++ cadastrar_carro.php
@@ -5,12 +5,12 @@
 
 require_once 'config.php';
 
-if(!isset($_SESSION['usuario_id'])){
+if (!isset($_SESSION['usuario_id'])) {
     header("location: login.php");
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     var_dump($_POST);
     $_usuario_id = $_SESSION['usuario_id'];
 
@@ -20,40 +20,98 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $foto = $_FILES['foto']['name'];
     $tmp  = $_FILES['foto']['tmp_name'];
 
-    move_uploaded_file($tmp, "uploads/".$foto);
+    move_uploaded_file($tmp, "uploads/" . $foto);
 
-   $stmt = $conn->prepare("INSERT INTO carros (descricao, valor, foto, usuario_id)
+    $stmt = $conn->prepare("INSERT INTO carros (descricao, valor, foto, usuario_id)
     VALUES (? , ?, ?, ?)");
-    $stmt->bind_param("sisi",$descricao, $valor, $foto, $_usuario_id );
+    $stmt->bind_param("sisi", $descricao, $valor, $foto, $_usuario_id);
     $stmt->execute();
 
-        header("Location: perfil.php");
-        exit();
-    
-    
+    header("Location: perfil.php");
+    exit();
 }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Carro</title>
+    <link rel="stylesheet" href="style/imports.css">
+    <link rel="stylesheet" href="style/cadastrar_carro.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+</head>
+
+
 <body>
 
-<h2>Cadastrar carro</h2>
+    <header>    
+        <h1>
+            Faça seu anúncio !
+        </h1>
+    </header>
 
-<form method="POST" enctype="multipart/form-data">
+    <main>
+        <form method="POST" enctype="multipart/form-data">
 
-Descrição:<br>
-<input type="text" name="descricao"><br><br>
+           <div class="campo">
+               <span>Descrição</span>
+                <input type="text" name="descricao">
+           </div>
 
-Valor:<br>
-<input type="number" name="valor"><br><br>
+            <div class="campo">
+                <span>Valor (da parcela)</span>
+                <input type="number" name="valor">
+            </div>
 
-Foto:<br>
-<input type="file" name="foto"><br><br>
+            <div class="campo">
+                <span>Foto</span>
+                <input class="btn_foto" type="file" name="foto">
+            </div>
 
-<button type="submit">Cadastrar</button>
+            <button class="btn_cadastrar" id="btn_cadastrar" type="submit">Cadastrar</button>
 
-</form>
+        </form>
+
+    </main>
+
+    <footer>
+        <div class="div_footer_conteiner">
+            <div class="conteiner_ES">
+                <div class="div_footer_identificacao_ES div_footer">
+                    <span>Criado Por Embrasoft &reg;</span>
+
+                </div>
+                <div class="div_footer_contatos_ES div_footer ">
+                    <span class="span_contato span_contato_whatsapp">Whasapp</span>
+                    <span class="span_contato span_contato_instagram">Instagran</span>
+                    <span class="span_contato span_contato_linkedin">LinkedIn</span>
+                </div>
+            </div>
+
+            <div class="conteiner_MC">
+                <div class="div_footer_identificacao_MC div_footer">
+                    <span>Mc Motors ltda &reg;</span>
+
+                </div>
+                <div class="div_footer_contatos_MC div_footer">
+                    <span class="span_contato span_contato_whatsapp_MC">Whasapp</span>
+                    <span class=" span_contato span_contato_intagram_mc">Instagran</span>
+                    <span class="span_contato span_contato_avaliacoes" id="span_avaliacao">Avalçiações do Google</span>
+                </div>
+            </div>
+        </div>
+
+
+        <script src="javascript/btn_rodapeES.js "></script>
+        <script src="javascript/btn_rodapeMC.js"></script>
+    </footer>
 
 </body>
+
+
+
 </html>
