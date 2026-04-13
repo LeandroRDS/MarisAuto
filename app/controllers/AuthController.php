30ad8b56a9812c61e9928b4b9936c825281f73c2
leandro <leandrords23012002@gmail.com>
2026-04-13 18:08:29 -0300

    fechando na loja  2

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -6,8 +6,19 @@ class AuthController{
         require_once  __DIR__ .'/../views/login.php';
     }
 
-    
+    public function autenticar(){
+        global $conn;
+            
+        $email = $_POST['email'] ?? '';
+        $senha = $_POST['senha'] ?? '';
+
+        $stmt = $conn->prepare("SELECT * FROM usuario WHERE emeail = ?");
+        $stmt->bli
+    }
+
 }
+
+
 
 
 ?>
